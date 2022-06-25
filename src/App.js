7d53fe8ba7c7f81ffaea1fb7192602884f8a21b1
Ponmani Kannan S <ponmani.kannan@siemens.com>
2022-06-25 21:50:53 +0530

    Added front end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
+
+
 import './App.css';
+import { HashRouter, Switch } from "react-router-dom";
+import { useEffect } from 'react';
+import Create from './Components/Create';
+import Home from './Components/Home';
+
+
+import MembersList from './Components/MembersList';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Edit from './Components/Edit';
+import  Navbar from './Components/Navbar';
+import About from './Components/About';
+import Contact from './Components/Contact';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Navbar/>
+      <header>
+        <h2>Members Dashboard</h2>
       </header>
+      <section>
+        <div className="container-fluid">
+          <article>
+            <Router>
+              <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/create' element={<Create />} />
+                <Route path='/edit/:id' element={<Edit />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/Contact' element={<Contact />} />
+              </Routes>
+            </Router>
+          </article>
+        </div>
+      </section>
     </div>
   );
 }
